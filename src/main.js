475e995aaba6cb23ae8956c5533ed9746efde8c6
Andy Pattenden <andyp@boughtbymany.com>
2020-09-29 12:38:02 +0100

    initial commit
    Adds svg and inline svg plugin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {InlineSvgPlugin} from 'vue-inline-svg';
+Vue.use(InlineSvgPlugin);
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <inline-svg
+      class="animation"
+      :src="require('@/assets/undraw_dog_walking.svg')"
+    />
   </div>
 </template>
 
