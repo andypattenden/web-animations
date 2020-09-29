9ca825ec76bd15f37cfa2206572e606a1df65d12
Andy Pattenden <andyp@boughtbymany.com>
2020-09-29 17:04:56 +0100

    animation improvements
    Improves animation.
    Tweaks SVG

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,101 +20,168 @@ export default {
   height: auto;
 }
 
+$walking-animation-speed: 0.75s;
+$dog-walking-animation-speed: $walking-animation-speed * 0.33333333;
+
 // Dog
 #dog {
-  &__leg {
-    $leg-animation-speed: 0.2s;
-
-    &--left {
-      &--front,
-      &--back {
-        transform-origin: top;
-        transform-box: fill-box;
-      }
-
-      &--front {
-        animation: dog-leg-left $leg-animation-speed linear infinite alternate;
-      }
-
-      &--back {
-        animation: dog-leg-left $leg-animation-speed linear infinite alternate-reverse;
-      }
+  @keyframes dog-leg-left {
+    from {
+      transform: rotateZ(20deg);
     }
 
-    &--right {
-      &-front,
-      &-back {
-        transform-box: fill-box;
+    to {
+      transform: rotateZ(60deg) translateY(60%);
+    }
+  }
+
+  &__ear-right,
+  &__ear--left {
+    transform-origin: center;
+    transform-box: fill-box;
+
+    @keyframes dog-ear {
+      from {
+        transform: rotateZ(0deg);
       }
 
-      &-front {
-        transform-origin: 30px 45px;
-        animation: dog-leg-right-front 0.2s linear infinite alternate-reverse;
-      }
-
-      &-back {
-        transform-origin: 30px 40px;
-        animation: dog-leg-right-back 0.2s linear infinite alternate;
+      to {
+        transform: rotateZ(30deg);
       }
     }
   }
 
-
-}
-
-@keyframes dog-leg-left {
-  from {
-    transform: rotateZ(0deg);
+  &__ear--left {
+    animation: dog-ear 1s ease-in-out infinite alternate-reverse;
   }
 
-  to {
-    transform: rotateZ(90deg) translateY(60%);
-  }
-}
-
-@keyframes dog-leg-right-front {
-  from {
-    transform: rotateZ(-20deg);
+  &__ear-right {
+    animation: dog-ear 1s ease-in-out infinite alternate;
   }
 
-  to {
-    transform: rotateZ(20deg) translateX(-5px);
-  }
-}
-
-@keyframes dog-leg-right-back {
-  from {
-    transform: rotateZ(-20deg) translateX(10px);
+  &__front-leg--left,
+  &__back-leg--left {
+    transform-origin: top;
+    transform-box: fill-box;
   }
 
-  to {
-    transform: rotateZ(20deg) ;
+  &__front-leg--left {
+    animation: dog-leg-left $dog-walking-animation-speed linear infinite alternate;
+  }
+
+  &__back-leg--left {
+    animation: dog-leg-left $dog-walking-animation-speed linear infinite alternate-reverse;
+  }
+
+  &__front-leg--right,
+  &__back-leg--right {
+    transform-box: fill-box;
+  }
+
+  &__front-leg--right {
+    transform-origin: 30px 45px;
+    animation: dog-leg-right-front $dog-walking-animation-speed linear infinite alternate-reverse;
+
+    @keyframes dog-leg-right-front {
+      from {
+        transform: rotateZ(-20deg);
+      }
+
+      to {
+        transform: rotateZ(20deg) translateX(-5px);
+      }
+    }
+  }
+
+  &__back-leg--right {
+    transform-origin: 30px 40px;
+    animation: dog-leg-right-back $dog-walking-animation-speed linear infinite alternate;
+
+    @keyframes dog-leg-right-back {
+      from {
+        transform: rotateZ(-20deg) translateX(10px);
+      }
+
+      to {
+        transform: rotateZ(20deg) ;
+      }
+    }
   }
 }
 
 // Man
 #man {
   &__upper-body {
-    animation: man-body 0.75s linear infinite alternate;
+    animation: man-body #{$walking-animation-speed / 2} linear infinite alternate-reverse;
     transform-box: fill-box;
+
+    @keyframes man-body {
+      from {
+        transform: translateY(0px);
+      }
+
+      to {
+        transform: translateY(20px);
+      }
+    }
   }
 
   &__arm {
     &--right {
-      animation: man-arm 1.5s linear infinite alternate;
+      animation: man-arm $walking-animation-speed linear infinite alternate;
       transform-origin: 30px 30px;
       transform-box: fill-box;
+    }
+
+    @keyframes man-arm {
+      from {
+        transform: rotateZ(22deg);
+      }
+
+      to {
+        transform: rotateZ(-22deg);
+      }
     }
   }
 
   &__foot--left,
   &__foot--right {
-    transform-origin: top;
     transform-box: fill-box;
   }
 
+  &__foot--right {
+    transform-origin: top left;
+    animation: man-foot-right $walking-animation-speed linear infinite alternate;
+
+    @keyframes man-foot-right {
+      from {
+        transform: rotateZ(-10deg);
+      }
+
+      to {
+        transform: rotateZ(30deg) translate(10px, -35px);
+      }
+    }
+  }
+
+  &__foot--left {
+    transform-origin: 0 20px;
+
+    // FIXME: this breaks the lower leg transform-origin
+    // animation: man-foot-left 1.5s linear infinite alternate;
+
+    @keyframes man-foot-left {
+      from {
+        transform: rotateZ(30deg);
+      }
+
+      to {
+        transform: rotateZ(-20deg) translate(-10px, 0);
+      }
+    }
+  }
+
   &__leg {
-    $leg-speed: 1.5s;
     $leg-timing: linear;
 
     &-upper--left,
@@ -127,96 +194,70 @@ export default {
 
     // Right leg
     &-upper--right {
-      animation: man-upper-leg-right $leg-speed $leg-timing infinite alternate-reverse;
+      transform-origin: 50px 20px;
+      animation: man-upper-leg-right $walking-animation-speed $leg-timing infinite alternate-reverse;
+
+      @keyframes man-upper-leg-right {
+        from {
+          transform: rotateZ(20deg);
+        }
+
+        to {
+          transform: rotateZ(-10deg);
+        }
+      }
     }
 
     &-lower--right {
-      transform-origin: top right;
-      animation: man-lower-leg-right $leg-speed $leg-timing infinite alternate-reverse;
+      transform-origin: 80% 40px ;
+      animation: man-lower-leg-right $walking-animation-speed $leg-timing infinite alternate-reverse;
+
+      @keyframes man-lower-leg-right {
+        from {
+          transform: rotateZ(-15deg) translate(-20px, 0px);
+        }
+
+        to {
+          transform: rotateZ(-70deg) translate(35px, 40px);
+        }
+      }
     }
 
 
     // Left Leg
     &-upper--left {
-      animation: man-upper-leg-left $leg-speed $leg-timing infinite alternate;
+      transform-origin: 25px 20px;
+      animation: man-upper-leg-left $walking-animation-speed $leg-timing infinite alternate;
+
+      @keyframes man-upper-leg-left {
+        from {
+          transform: rotateZ(20deg);
+        }
+
+        to {
+          transform: rotateZ(-10deg);
+        }
+      }
     }
 
     &-lower--left {
-      // transform-origin: top right;
-      animation: man-lower-leg-left $leg-speed $leg-timing infinite alternate;
+      transform-origin: 33px 10px;
+      animation: man-lower-leg-left $walking-animation-speed $leg-timing infinite alternate;
+
+      @keyframes man-lower-leg-left {
+        from {
+          transform: rotateZ(40deg) translateX(-13px);
+        }
+
+        to {
+          transform: rotateZ(-20deg) translateX(10px);
+        }
+      }
     }
-  }
-}
-
-@keyframes man-arm {
-  from {
-    transform: rotateZ(22deg);
-  }
-
-  to {
-    transform: rotateZ(-22deg);
-  }
-}
-
-@keyframes man-body {
-  from {
-    transform: translateY(-2px);
-  }
-
-  to {
-    transform: translateY(2px);
-  }
-}
-
-
-// Left Leg
-@keyframes man-upper-leg-left {
-  from {
-    transform: rotateZ(20deg);
-  }
-
-  to {
-    transform: rotateZ(-10deg) translate(0, -20px);
-  }
-}
-
-@keyframes man-lower-leg-left {
-  from {
-    transform: rotateZ(45deg) translate(-10px, 25px);
-  }
-
-  to {
-    transform: rotateZ(-15deg) translate(10px, -20px);
-  }
-}
-
-
-// Right leg
-@keyframes man-upper-leg-right {
-  from {
-    transform: rotateZ(20deg);
-  }
-
-  to {
-    transform: rotateZ(-10deg);
-  }
-}
-
-@keyframes man-lower-leg-right {
-  from {
-    transform: rotateZ(-10deg) translate(-40px, -25px);
-  }
-
-  to {
-    transform: rotateZ(-70deg) translate(27px, -25px);
   }
 }
 
 // Background
-#building {
-  animation: background 60s linear infinite;
-}
-
 @keyframes background {
   from {
     transform: translateX(100%);
@@ -227,22 +268,35 @@ export default {
   }
 }
 
+#building,
+#tree,
+#plant,
+#cloud--large,
+#cloud--small {
+  animation-name: background;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+
+#building {
+  animation-duration: $walking-animation-speed * 80; // 60s
+}
 
 #tree{
-  animation: background 15s linear infinite;
+  animation-duration: $walking-animation-speed * 20; // 15s
 }
 
 #plant {
-  animation: background 10s linear infinite;
+  animation-duration: $walking-animation-speed * 13.333333; // 10s
 }
 
 #cloud {
   &--large {
-    animation: background 120s linear infinite;
+    animation-duration: $walking-animation-speed * 160; // 120s
   }
 
- &--small {
-    animation: background 90s linear infinite;
+  &--small {
+    animation-duration: $walking-animation-speed * 120; // 90s
   }
 }
 </style>
