43209eca82bd874c86515c2838d2e730b33e36b4
Andy Pattenden <andyp@boughtbymany.com>
2020-10-02 16:59:48 +0100

    eslint allow console

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['@boughtbymany/eslint-config-bbm/vue'],
+
+  rules: {
+    // Override base eslint config to allow console logging
+    'no-console': [
+      'error',
+      { allow: ['info', 'warn', 'error', 'debug', 'groupCollapsed', 'groupEnd'] },
+    ],
+  },
 }
